62463ea13984e687b8b966b4d15fbaa2c8088d1d
Gomathy V <sreekrishm789@gmail.com>
2026-05-18 21:08:01 +0530

    trained working model

---
+++ real-estate-backend/train.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import joblib
+
+# Load dataset
+df = pd.read_csv("train.csv")
+
+# Features used for prediction
+X = df[['OverallQual', 'GrLivArea', 'GarageCars',
+        'TotalBsmtSF', 'FullBath', 'YearBuilt']]
+
+# Target column
+y = df['SalePrice']
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Create model
+model = LinearRegression()
+
+# Train model
+model.fit(X_train, y_train)
+
+# Save model
+joblib.dump(model, 'house_price_model.pkl')
+
+print("Model trained and saved successfully")
